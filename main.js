3546b5d43f8ec4b841934606e99d801fcb30e18e
Lazar Radenkovic <lazarvelickovic@Stefans-MacBook-Air-6.local>
2025-11-30 16:59:06 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Webflow scripts loaded successfully!');
+
+// Ovde ćemo pisati vaše funkcije
+function init() {
+    console.log('Inicijalizacija...');
+}
+
+document.addEventListener('DOMContentLoaded', init);
